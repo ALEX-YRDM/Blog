09f73ab2d473a1c5c1109121c2e1d86c482b1e6d
zbq <yirendm202022@gmail.com>
2022-10-13 18:05:50 +0800

    first commit:create SpringBoot project and import front pages.Some pictures and .css files may not work well because of thymeleaf,it will be fixed soon.

---
+++ README.md
@@ -0,0 +1,4 @@
+# 工程简介
+Semantic-ui+Springboot+JPA+MySQL 个人博客搭建
+# 延伸阅读
+
---
+++ src/main/java/com/zhang/blog/web/IndexController.java
@@ -0,0 +1,24 @@
+package com.zhang.blog.web;
+
+import com.zhang.blog.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author zbq
+ * @date 2022/10/13 14:40
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        //int i=9/0;
+        /*String blog=null;
+        if(blog==null){
+            throw new NotFoundException("blog doesn't exist!");
+        }*/
+        //System.out.println("----------index--------------");
+        return "index";
+    }
+}
